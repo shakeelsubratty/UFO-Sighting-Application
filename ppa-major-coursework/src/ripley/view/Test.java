af00b1d16d1094a815e264b51c4fb9b12cc37c98
Shakeel Subratty <shakeel.subratty@gmail.com>
2017-03-07 18:09:37 +0000

    Added Welcome Panel.

--- ppa-major-coursework/src/ripley/view/Test.java
+++ ppa-major-coursework/src/ripley/view/Test.java
@@ -3,6 +3,8 @@ package ripley.view;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import api.ripley.Ripley;
+
 /**
  * Class used for testing panels - REMOVE BEFORE FINAL SUBMISSION
  * 
@@ -13,15 +15,23 @@ public class Test
 {
 	public static void main(String[] args)
 	{
+		String privateKey = "10tLI3GWut+yVD6ql2OMtA==";
+		String publicKey = "tBgm4pVo/g/VqL46EnH7ew==";
+		
+		//Create instance of Ripley
+		Ripley ripley = new Ripley(privateKey, publicKey);
+		
 		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel statsPanel = new StatisticsPanel("Alex", "123");
+		JPanel welcomePanel = new WelcomePanel(ripley);
 		
 		frame.add(statsPanel);
+		frame.add(welcomePanel);
 		
 		frame.pack();
 		frame.setVisible(true);
 		
-		frame.remove(statsPanel);
 	}
 }
---
+++ ppa-major-coursework/src/ripley/view/WelcomePanel.java
@@ -0,0 +1,43 @@
+package ripley.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import api.ripley.Ripley;
+
+public class WelcomePanel extends JPanel 
+{
+	public WelcomePanel(Ripley ripley)
+	{
+		initialise(ripley);
+	}
+	
+	private void initialise(Ripley ripley)
+	{
+		//BorderLayout manager
+		setPreferredSize(new Dimension(450, 450));
+		this.setLayout(new BorderLayout());
+	
+		JLabel text = new JLabel();
+		String str = "Welcome to the Ripley API v" + ripley.getVersion()
+				+ " <br>Please select from the range of dates above, "
+				+ " <br> in order to begin analysing UFO sighting data"
+				+ " <br>"
+				+ " <br> Date selected"
+				+ " <br>"
+				+ " <br> Grabbing data"
+				+ " <br>"
+				+ " <br> Data grabbed in "
+				+ " <br>"
+				+ " <br> <b>Please now interact with the buttons below</b>";
+		text.setHorizontalAlignment(SwingConstants.CENTER);
+		text.setText("<html> <div style='text-align: center;'>" + str + "</div>");
+		
+		add(text, BorderLayout.CENTER);
+	}
+
+}
